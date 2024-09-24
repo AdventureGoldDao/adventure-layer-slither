da93e34f70800d57ad7defefc6dde8a7a15bde64
liuwill <liuwill@live.com>
2024-09-24 11:35:31 +0800

    feat: add connect wallet button and config

--- snake-game/src/App.tsx
+++ snake-game/src/App.tsx
@@ -1,8 +1,114 @@
-import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react';
+import { ChakraProvider, Container, Heading, Button } from "@chakra-ui/react";
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { Provider } from "react-redux";
 import CanvasBoard from "./components/CanvasBoard";
 import ScoreCard from "./components/ScoreCard";
 import store from "./store";
+
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+
+const btnCls = css`
+align-items: center;
+border-color: #f39b4b;
+border-radius: 6px;
+display: flex;
+flex-direction: row;
+height: 32px;
+justify-content: center;
+margin-right: 15px;
+margin-left: 15px;
+width: 160px;
+
+background: #f39b4b;
+color: #000;
+border-color: #f39b4b;
+
+&.connect, &.connect-btn, &:hover {
+  background: #f39b4b;
+  color: #000;
+  border-color: #f39b4b;
+}
+`
+
+const styles: any = {
+  connect: btnCls,
+  menuBox: css`
+    width: 100%;
+    height: 58px;
+    background: #1F1A15;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  logoBox: css`
+    margin-left: 46px;
+    width: 28px;
+    height: 28px;
+  `,
+  logo: css`
+    width: 100%;
+    height: 100%;
+  `,
+  menu: css`
+    width: 309px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  menuItem: css`
+    color: #fff;
+    font-size: 14px;
+    text-decoration: none;
+    font-family: 'NeueHaasDisplayMediu';
+  `,
+}
+
+function WalletButton() {
+  const [rendered, setRendered] = useState("");
+  const { ens } = useLookupAddress('');
+  const { account, activateBrowserWallet, deactivate, error } = useEthers();
+
+  useEffect(() => {
+    if (ens) {
+      setRendered(ens);
+    } else if (account) {
+      setRendered(shortenAddress(account));
+    } else {
+      setRendered("");
+    }
+  }, [account, ens, setRendered]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error while connecting wallet:", error.message);
+    }
+  }, [error]);
+
+  return (
+    <Button
+      css={btnCls}
+      // className={styles['connect']}
+      variant="contained" size="medium"
+      // style={{ padding: '4px 5px', marginBottom: '12px' }}
+      onClick={() => {
+        if (!account) {
+          activateBrowserWallet();
+        } else {
+          deactivate();
+        }
+      }}>
+      <div className={styles['connect-btn']}>
+        {rendered === "" && "Connect AGLD"}
+        {rendered !== "" && rendered}
+      </div>
+    </Button>
+  );
+}
 
 const App = () => {
   return (
@@ -11,6 +117,9 @@ const App = () => {
         <Container maxW="container.lg" centerContent>
           <Heading as="h1" size="xl">SNAKE GAME</Heading>
           <ScoreCard />
+          <div style={{marginBottom: "12px"}}>
+            <WalletButton />
+          </div>
           <CanvasBoard height={600} width={1000} />
         </Container>
       </ChakraProvider>
--- snake-game/src/index.tsx
+++ snake-game/src/index.tsx
@@ -2,10 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
+// / import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { DAppProvider, Mainnet, Sepolia, ChainId, DEFAULT_SUPPORTED_CHAINS } from "@usedapp/core";
+// import { getDefaultProvider } from 'ethers'
+
+import { AdventureLayer as l2 } from './config'
+
+const getAddressLink = (explorerUrl: string) => (address: string) => `${explorerUrl}/address/${address}`
+const getTransactionLink = (explorerUrl: string) => (txnId: string) => `${explorerUrl}/tx/${txnId}`
+
+const AdventureLayer = {
+  chainId: l2.chainId,
+  rpcUrl: l2.rpcUrl,
+  wssUrl: l2.wssUrl,
+  chainName: l2.chainName,
+  isTestChain: false,
+  isLocalChain: false,
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  blockExplorerUrl: l2.blockExplorerUrl,
+  getExplorerAddressLink: getAddressLink(l2.blockExplorerUrl),
+  getExplorerTransactionLink: getTransactionLink(l2.blockExplorerUrl),
+}
+
+const config: any = {
+  // readOnlyChainId: Sepolia.chainId,
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: Mainnet.rpcUrl,
+    [Sepolia.chainId]: Sepolia.rpcUrl,
+    [AdventureLayer.chainId]: AdventureLayer.rpcUrl,
+  },
+  // supportedChains: [ChainId.Mainnet, ChainId.Goerli, ChainId.Kovan, ChainId.Rinkeby, ChainId.Ropsten, ChainId.Arbitrum, ChainId.Sepolia, AdventureLayer.chainId],
+  networks: [...DEFAULT_SUPPORTED_CHAINS, AdventureLayer,], // AdventureLocal2
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
